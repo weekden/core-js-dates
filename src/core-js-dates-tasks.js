d37b010b65e3adabc35ab549cabfb048f582dac1
DenisNedelko <dnsndlk@gmail.com>
2025-03-20 23:25:58 +0300

    feat: add solition 7-8 2025-03-20 23:25:58

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -132,8 +132,11 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  return (
+    new Date(date) >= new Date(period.start) &&
+    new Date(date) <= new Date(period.end)
+  );
 }
 
 /**
@@ -147,8 +150,8 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  return new Date(date).toLocaleString('en', { timeZone: 'utc' });
 }
 
 /**
